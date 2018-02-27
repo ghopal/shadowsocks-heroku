37e99ed72cf93117ac89d0526f3f8c4be7d00fa2
ghopal <36897176+ghopal@users.noreply.github.com>
2018-02-28 01:13:40 +0700

    Update server.js

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ if (['', 'null', 'table'].includes(METHOD.toLowerCase())) {
 
 const server = http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('asdf.');
+  res.end('Hai beb..');
 });
 
 const wss = new WebSocketServer({ server });
